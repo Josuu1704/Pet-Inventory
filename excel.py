2cd082f91c20a4a10295a966ae3ef2e1bc00d1c2
rodgar00 <rodrigogarciaperez00@gmail.com>
2026-03-02 13:31:43 +0100

    Parte Rodrigo

--- excel.py
+++ excel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def guardar_excel(tabla_inventario, nombre_archivo="Inventario.xlsx"):
     try:
-        tabla_inventario.to_excel(nombre_archivo, index=False) #index borra la columna extra para que quede mas limpio
+        tabla_inventario.to_excel(nombre_archivo, index=False)
         print(f"Archivo {nombre_archivo} guardado correctamente.")
     except Exception as e:
         print(f"Hubo un error al guardar: {e}")
